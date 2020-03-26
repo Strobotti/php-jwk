04bed6611f0914fd99d5396106051fff72202c8a
Juha Jantunen <juha@strobotti.com>
2020-03-26 22:44:03 +0400

    Increase test-coverage

--- tests/Key/RsaTest.php
+++ tests/Key/RsaTest.php
@@ -53,4 +53,22 @@ final class KeyTest extends TestCase
 EOT
         ];
     }
+
+    public function testToString(): void
+    {
+        $json = <<<'EOT'
+{
+    "kty": "RSA",
+    "use": "sig",
+    "alg": "RS256",
+    "kid": "86D88Kf",
+    "n": "iGaLqP6y-SJCCBq5Hv6pGDbG_SQ11MNjH7rWHcCFYz4hGwHC4lcSurTlV8u3avoVNM8jXevG1Iu1SY11qInqUvjJur--hghr1b56OPJu6H1iKulSxGjEIyDP6c5BdE1uwprYyr4IO9th8fOwCPygjLFrh44XEGbDIFeImwvBAGOhmMB2AD1n1KviyNsH0bEB7phQtiLk-ILjv1bORSRl8AK677-1T8isGfHKXGZ_ZGtStDe7Lu0Ihp8zoUt59kx2o9uWpROkzF56ypresiIl4WprClRCjz8x6cPZXU2qNWhu71TQvUFwvIvbkE1oYaJMb0jcOTmBRZA2QuYw-zHLwQ",
+    "e": "AQAB"
+}
+EOT;
+
+        $key = Rsa::createFromJSON($json);
+
+        $this->assertSame($json, "$key");
+    }
 }
