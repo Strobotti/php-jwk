4ae5aa68267f427a3787c2239697638344c6e104
Juha Jantunen <juha@strobotti.com>
2024-04-30 08:25:30 +0300

    ci: use cobertura instead of clover and update phpunit (#13)
    * ci: use covertura coverage instead of clover
    
    * ci: add coverage-badge

--- tests/Util/Base64UrlConverterTest.php
+++ tests/Util/Base64UrlConverterTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Strobotti\JWK\Key\Tests;
+namespace Strobotti\JWK\Tests\Util;
 
 use PHPUnit\Framework\TestCase;
 use Strobotti\JWK\Util\Base64UrlConverter;
@@ -19,10 +19,10 @@ final class Base64UrlConverterTest extends TestCase
     {
         $converter = new Base64UrlConverter();
 
-        static::assertSame($expected, $converter->decode($input));
+        $this->assertSame($expected, $converter->decode($input));
     }
 
-    public function provideDecode(): \Generator
+    public static function provideDecode(): \Generator
     {
         yield [
             'expected' => '/a+quick+brown+fox/jumped-over/the_lazy_dog/',
@@ -40,7 +40,7 @@ final class Base64UrlConverterTest extends TestCase
         static::assertSame($expected, $converter->encode($input));
     }
 
-    public function provideEncode(): \Generator
+    public static function provideEncode(): \Generator
     {
         yield [
             'expected' => 'L2ErcXVpY2srYnJvd24rZm94L2p1bXBlZC1vdmVyL3RoZV9sYXp5X2RvZy8',
